8b388aca96eea1ec035220f14be0ae414788776e
Rizwan-zaman38 <rizwanzaman38@gmail.com>
2025-01-03 00:02:42 -0700

    updated

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,4 +1,5 @@
 import { User } from "../modal/usermodal.js";
+import bcrypt from 'bcrypt'
 
 export const register = async(req,res)=>{
    try {
@@ -8,11 +9,12 @@ export const register = async(req,res)=>{
         // return res.send('Please fill all the fields')
         return res.status(400).json({message: "Please fill all the fields."});
     }
+const hashpassword = await bcrypt.hash(password,10)
 
     const user = await User.create({
         name,
         email,
-        password
+        password: hashpassword
     });
 
     res.json({
@@ -24,4 +26,34 @@ export const register = async(req,res)=>{
        console.error(error);
        return res.status(500).json({message: "Server error"})
    }
+}
+
+export const login = async(req,res)=>{
+    try {
+        const{email,password} = req.body;
+
+    if(!email||!password){
+        return res.status(400).json({message: "Please fill all the fields"});
+    }
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(404).json({message: "User not found."});
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.status(401).json({message: "Invalid credentials."});
+    }
+ 
+    res.json({
+        message: 'Loggin successfully.',
+        user,
+    })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: "Server error."});
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { register } from '../controller/usercontroller.js';
+import { login, register } from '../controller/usercontroller.js';
 
 const routes = express.Router();
 
 routes.route("/register").post(register);
+routes.route("/login").post(login);
+
 
 export default routes;
 
